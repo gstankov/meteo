9ed0b2f6bf3368e7730fd5b914970d862491edc0
gstankov <gstankov1982@gmail.com>
2017-07-23 10:51:31 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Meteo</title>
+        <style type="text/css">
+            body {
+                padding: 0px; margin: 0px;
+                background: #000;
+                width: 100%; text-align: center;
+            }
+            body img {
+                width: 632px;
+                border: 1px solid green;
+                padding: 5px; margin: 2px;
+            }
+        </style>
+        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
+    </head>
+    <body>
+        <img src="http://api.sat24.com/" /><br />
+        <img src="http://vrijeme.hr/irc-anim.gif" /><br />
+        <img src="http://vrijeme.hr/kradar-anim.gif" /><br />
+        <img src='http://api.sat24.com/animated/EU/visual/1/Central%20European%20Standard%20Time/3118322' /><br />
+        <img src="https://images.lightningmaps.org/blitzortung/europe/index.php?animation=5" />
+    </body>
+</html>
+
